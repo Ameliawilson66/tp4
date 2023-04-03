7e15b6fc202782cc891f6342532d305a58d18d16
vynckewilsona <vynckewilsona@educ.college-montreal.qc.ca>
2023-04-03 09:08:13 -0400

    tp4 - code cercle

--- actual tp4.py
+++ actual tp4.py
@@ -9,25 +9,13 @@ SCREEN_HEIGHT = 600
 
 COLORS = [arcade.color.BLUE, arcade.color.RED, arcade.color.ORANGE, arcade.color.YELLOW, arcade.color.GREEN, arcade.color.ROYAL_PURPLE, arcade.color.MAGENTA]
 
-class Balle():
-    def __init__(self, r,x,y,c):
-        self.rayon = r
-        self.centre_x = x
-        self.centre_y = y
-        self.color = (c)
-
-    def draw(self):
-        #paramètres du cercle(centre(axe des x), centre(axe des y), rayon, couleur)
-        arcade.draw_circle_filled(self.centre_x, self.centre_y, self.rayon, self.color)
-
 class Rectangle():
-    def __init__(self, x,y,c):
+    def __init__(self, x,y,c, w, h):
         self.centre_x = x
         self.centre_y = y
+        self.width = w
+        self.height = h
         self.color = c
-        self.width = random.randint(10, 30)
-        self.height = random.randint(10, 30)
-        self.color = COLORS
         self.tilt_angle = 0
 
     def draw(self):
@@ -39,26 +27,13 @@ class MyGame(arcade.Window):
     #ouverture de la fenêtre + nom: exercice 1
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
-        self.liste_cercles = []
         self.liste_rectangle = []
-
-
-
-    def setup(self):
-        # remplir la fenêtre avec 20 cercles(position et couleur aléatoire)
-        for _ in range(0):
-            rayon = random.randint(10, 30)
-            center_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
-            center_y = random.randint(0 + rayon, SCREEN_HEIGHT - rayon)
-            color = random.choice(COLORS)
-            cercle= Balle(rayon, center_x, center_y, color)
-            self.liste_cercles.append(cercle)
 
     def draw(self):
         # remplir la fenêtre avec 20 cercles(position et couleur aléatoire)
         for _ in range(1):
-            center_x = random.randint(0 + random.randint(SCREEN_WIDTH) - width)
-            center_y = random.randint(0 + random.randint(SCREEN_HEIGHT) - height)
+            center_x = random.randint(0 + random.randint(SCREEN_WIDTH))
+            center_y = random.randint(0 + random.randint(SCREEN_HEIGHT))
             color = random.choice(COLORS)
             rectangle = Rectangle(center_x, center_y, color)
             self.liste_rectangle.append(rectangle)
@@ -66,14 +41,11 @@ class MyGame(arcade.Window):
     def on_draw(self):
         #dessine le cerle lorsque la fenêtre est ouverte
         arcade.start_render()
-
-        for cercle in self.liste_cercles:
-            cercle.draw()
         for rectangle in self.liste_rectangle:
             rectangle.draw()
 
     def on_mouse_press(self, x: int, y: int , button: int, modifiers: int):
-        if button == arcade.MOUSE_BUTTON_LEFT:
+        if button == arcade.MOUSE_BUTTON_RIGHT:
             for rectangle in self.liste_rectangle:
                 self.liste_rectangle.append(rectangle)
 
@@ -86,7 +58,6 @@ class MyGame(arcade.Window):
 
 def main():
     my_game = MyGame()
-    my_game.setup()
 
     arcade.run()
 
--- main.py
+++ main.py
@@ -10,11 +10,30 @@ SCREEN_HEIGHT = 600
 COLORS = [arcade.color.BLUE, arcade.color.RED, arcade.color.ORANGE, arcade.color.YELLOW, arcade.color.GREEN, arcade.color.ROYAL_PURPLE, arcade.color.MAGENTA]
 
 class Cercle():
-    def __init__(self, r,x,y,c):
+    #rayon, x, y, change_x, change_y, color
+    def __init__(self, r, x, y, v_x, v_y, c):
         self.rayon = r
         self.centre_x = x
         self.centre_y = y
+        self.change_x = v_x
+        self.change_y = v_y
         self.color = (c)
+
+    def on_update(self):
+        self.centre_x += self.change_x
+        self.centre_y += self.change_y
+
+        if self.centre_x < self.rayon:
+            self.change_x *= -1
+
+        if self.centre_x > SCREEN_WIDTH - self.rayon:
+            self.change_x *= -1
+
+        if self.centre_y < self.rayon:
+            self.change_y *= -1
+
+        if self.centre_y > SCREEN_HEIGHT - self.rayon:
+            self.change_y *= -1
 
     def draw(self):
         #paramètres du cercle(centre(axe des x), centre(axe des y), rayon, couleur)
@@ -29,27 +48,30 @@ class MyGame(arcade.Window):
 
 
 
-    def setup(self):
-        # remplir la fenêtre avec 20 cercles(position et couleur aléatoire)
-        for _ in range(20):
-            rayon = random.randint(20, 40)
-            center_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
-            center_y = random.randint(0 + rayon, SCREEN_HEIGHT - rayon)
-            color = random.choice(COLORS)
-            cercle= Cercle(rayon, center_x, center_y, color)
-            self.liste_cercles.append(cercle)
-
     def on_draw(self):
-        #dessine le cerle lorsque la fenêtre est ouverte
         arcade.start_render()
-
         for cercle in self.liste_cercles:
             cercle.draw()
 
 
+    def on_update(self, delta_time: float):
+        #dessine le cerle lorsque la fenêtre est ouverte
+        arcade.start_render()
+        for cercle in self.liste_cercles:
+            cercle.on_update()
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            rayon = random.randint(15, 50)
+            change_x = random.randint(1, 10)
+            change_y = random.randint(1, 10)
+            color = random.choice(COLORS)
+            cercle = Cercle(rayon, x, y, change_x, change_y, color)
+            self.liste_cercles.append(cercle)
+
 def main():
     my_game = MyGame()
-    my_game.setup()
 
     arcade.run()
 
